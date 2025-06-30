ce4c3497d96ae1f28e5053564e845da507ae03c3
JaumeG13 <jaumegenaro02@gmail.com>
2025-06-30 20:28:13 +0200

    Update import.js

--- firestore-import/import.js
+++ firestore-import/import.js
@@ -1,22 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { readFile } from "fs/promises";
+const data = await readFile("./drivers.json", "utf8");
+const drivers = JSON.parse(data);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB48KkdlQb794w2euzJWHwkUfz1xXbBQ38",
-  authDomain: "circuit-f666e.firebaseapp.com",
-  projectId: "circuit-f666e",
+    apiKey: "AIzaSyB48KkdlQb794w2euzJWHwkUfz1xXbBQ38",
+    authDomain: "circuit-f666e.firebaseapp.com",
+    projectId: "circuit-f666e",
+    storageBucket: "circuit-f666e.firebasestorage.app",
+    messagingSenderId: "829065746483",
+    appId: "1:829065746483:web:921256dd57c9cd1112eb46"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const data = await readFile("./drivers.json", "utf8");
-const drivers = JSON.parse(data);
-
 async function importDrivers() {
   for (const driver of drivers) {
-    const ref = doc(db, "drivers", String(driver["steam-id"]));
+    const ref = doc(db, "drivers", String(driver["steam-id"])); // Use steam ID as doc ID
 
     await setDoc(ref, {
       name: driver.name,
@@ -32,5 +34,3 @@ async function importDrivers() {
     console.log(`✅ Imported: ${driver.name}`);
   }
 }
-
-importDrivers().catch(console.error);
